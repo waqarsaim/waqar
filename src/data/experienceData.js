f9a9be63a10d003f177d9085694236d5e4f463f3
waqar <waqarsaim1@gmail.com>
2023-09-15 17:13:50 +0500

    Initial commit

---
+++ src/data/experienceData.js
@@ -0,0 +1,24 @@
+export const experienceData = [
+  {
+    id: 1,
+    company: 'CINQDev',
+    jobtitle: 'Full Stack Developer',
+    startYear: '2023',
+    endYear: 'Present',
+  },
+  {
+    id: 2,
+    company: 'LEADconcept Software House',
+    jobtitle: 'Frontend Developer(React.js, Redux)',
+    startYear: '23-01-2020',
+    endYear: 'Present',
+  },
+
+  {
+    id: 3,
+    company: 'Stack Tech',
+    jobtitle: 'Frontend Developer(JavaScript, React.js)',
+    startYear: '15-03-2019',
+    endYear: '15-01-2020',
+  },
+]
---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/Muhammad Waqar.pdf'
+import waqarr from '../assets/png/waqarr.png'
+export const headerData = {
+    name: 'Muhammad Waqar',
+    title: "Full-Stack Developer",
+    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    image: waqarr,
+    resumePdf: resume
+}
